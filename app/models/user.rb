5bf18612dbc9d5241d30b73a68ee59bc3225f5d6
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-19 17:49:05 +0900

    カタカナ入力のみを許容する実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
   validates :nickname, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :birthday, presence: true
-  validates :kana_first_name, presence: true
-  validates :kana_last_name, presence: true
+  validates :kana_first_name, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :kana_last_name, presence: true, format: { with: KATAKANA_REGEXP }
 
 
 
