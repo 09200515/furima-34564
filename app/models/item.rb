4de77c7a3a2bd386dd8aa896d91976b929044419
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-22 09:47:28 +0900

    ActiveHashを表示する実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,17 +4,21 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :status_id
     validates :price
-    validates :delivery_fee_id
-    validates :shipping_area_id
-    validates :shippment_id
+    
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :delivery_fee_id
+      validates :shipping_area_id
+      validates :shippment_id
+    end
   end
+
 
   belongs_to :user
   has_one_attached :image
-  
+
   belongs_to :prefecture
   belongs_to :category
   belongs_to :status
