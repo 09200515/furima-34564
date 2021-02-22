6bdb3f82945c6ce597eeaf15349b290c2b982a7b
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-22 17:39:21 +0900

    単体テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,9 +4,9 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: {only_integer: true, greter_than_or_equal_to: 300, less_than_or_equal_to: 99999999}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :image
-    with_options numericality: { other_than: 1, message: ' select' } do
+    with_options numericality: { other_than: 1, message: 'select' } do
       validates :category_id
       validates :status_id
       validates :delivery_fee_id
