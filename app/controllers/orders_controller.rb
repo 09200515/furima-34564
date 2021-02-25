5e498f4b193ee3f5e0e8fcb465948db6e8327b56
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-25 13:01:58 +0900

    エラーハンドリングの導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,6 +12,7 @@ class OrdersController < ApplicationController
       redirect_to root_path
       return
     else
+      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -20,7 +21,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_delivery).permit(:postalcord, :shipping_area_id, :city, :address_line1, :address_line2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:order_delivery).permit(:postal_cord, :shipping_area_id, :city, :address_line1, :address_line2, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item
