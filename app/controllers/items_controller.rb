ce723a0788344cf382f8b5d8be9ecc20d0e8e4b7
09200515 <a6t3lka23tsc8kg@i.softbank.jp>
2021-02-26 18:29:27 +0900

    ! マークを全角から半角へ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,7 +51,7 @@ class ItemsController < ApplicationController
   end
 
   def redirect
-    if current_user.id ！= @item.user_id || current_user.id == @item.user_id && @item.order.present?
+    if current_user.id != @item.user_id || current_user.id == @item.user_id && @item.order.present?
       redirect_to root_path
     end
   end
